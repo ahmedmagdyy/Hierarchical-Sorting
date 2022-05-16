0819c27af6f7a2bca8f085a21f27175327cd3b43
ahmed magdy <ahmedmagdy2016@gmail.com>
2022-05-16 12:20:22 +0200

    cleanup and move parts of code into separated function

--- sort.js
+++ sort.js
@@ -1,64 +1,24 @@
 const { argv } = require("node:process");
 const fs = require("fs");
+const path = require("node:path");
 
 const TOTAL_SIGN = "$total";
 const COLUMN_SEPERATOR = "|";
 const LINE_SEPERATOR = "\n";
 
 function hierarchicalSort(rows, sortColumnName) {
-  // console.log({ rows, sortColumnName });
   const rowsCopy = rows;
-  const lengthOfRows = rowsCopy.length;
-
-  let mainCategoriesWithTotal = [];
-  let rowsContainsNestedTotalProp = [];
-  let headerWithTotalProp = [rowsCopy[0]];
-  const rowsWithoutTotalProp = new Map();
-
   const sortColumnIndex = rowsCopy[0]
     .split(COLUMN_SEPERATOR)
     .indexOf(sortColumnName);
 
-  // get count of properties and metric columns
-  const propertiesNumber = rowsCopy[0]
-    .split(COLUMN_SEPERATOR)
-    .reduce((prev, acc) => {
-      if (acc.includes("property")) {
-        prev = prev + 1;
-      }
-      return prev;
-    }, 0);
-  console.log({ propertiesNumber });
-
-  // 0 is header so we start from 1
-  for (let i = 1; i < lengthOfRows; i++) {
-    const singleRow = rowsCopy[i];
-    const singleRowSplitted = singleRow.split(COLUMN_SEPERATOR);
-    // extract rows have total property
-    if (singleRow.includes(TOTAL_SIGN)) {
-      if (singleRowSplitted[0] === TOTAL_SIGN) {
-        headerWithTotalProp.push(singleRow);
-      } else if (singleRowSplitted[1] === TOTAL_SIGN) {
-        mainCategoriesWithTotal.push(singleRow);
-      } else {
-        rowsContainsNestedTotalProp.push(singleRow);
-      }
-    } else {
-      // create array of nested properties / categories -> womens footwear|shoes
-      // extract rows have nested properties but not total property
-      const itemName = singleRowSplitted
-        .slice(0, propertiesNumber - 1)
-        .join(COLUMN_SEPERATOR);
-      if (rowsWithoutTotalProp.has(itemName)) {
-        rowsWithoutTotalProp.set(itemName, [
-          ...rowsWithoutTotalProp.get(itemName),
-          singleRow,
-        ]);
-      } else {
-        rowsWithoutTotalProp.set(itemName, [singleRow]);
-      }
-    }
-  }
+  const {
+    mainCategoriesWithTotal,
+    rowsContainsNestedTotalProp,
+    headerWithTotalProp,
+    rowsWithoutTotalProp,
+    propertiesCount,
+  } = categorizeRowsBasedOnCategory(rowsCopy);
 
   // sort items by metric column
   rowsContainsNestedTotalProp.sort(
@@ -87,13 +47,13 @@ function hierarchicalSort(rows, sortColumnName) {
         if (category === mainCategoryName) {
           if (
             rowHasNestedTotalProp
-              .slice(0, propertiesNumber - 1)
+              .slice(0, propertiesCount - 1)
               .includes(TOTAL_SIGN)
           ) {
             sortedItems.push(rowsContainsNestedTotalProp[j]);
           } else {
             const property = rowHasNestedTotalProp
-              .slice(0, propertiesNumber - 1)
+              .slice(0, propertiesCount - 1)
               .join(COLUMN_SEPERATOR);
             sortedItems.push(
               rowsContainsNestedTotalProp[j],
@@ -107,6 +67,7 @@ function hierarchicalSort(rows, sortColumnName) {
         }
       }
     } else {
+      // if we don't have any nested categories with total
       sortedItems.push(
         ...getChildrensByMainCategory(
           rowsWithoutTotalProp,
@@ -116,7 +77,64 @@ function hierarchicalSort(rows, sortColumnName) {
       );
     }
   }
-  console.log({ sortedItems });
+  return sortedItems;
+}
+
+function categorizeRowsBasedOnCategory(rows) {
+  const rowsCopy = rows;
+  const lengthOfRows = rowsCopy.length;
+
+  const rowsContainsNestedTotalProp = [];
+  const headerWithTotalProp = [rowsCopy[0]];
+  const rowsWithoutTotalProp = new Map();
+  const mainCategoriesWithTotal = [];
+
+  // get count of properties and metric columns
+  const propertiesCount = rowsCopy[0]
+    .split(COLUMN_SEPERATOR)
+    .reduce((prev, acc) => {
+      if (acc.includes("property")) {
+        prev = prev + 1;
+      }
+      return prev;
+    }, 0);
+
+  // 0 is header so we start from 1
+  for (let i = 1; i < lengthOfRows; i++) {
+    const singleRow = rowsCopy[i];
+    const singleRowSplitted = singleRow.split(COLUMN_SEPERATOR);
+    // extract rows have total property
+    if (singleRow.includes(TOTAL_SIGN)) {
+      if (singleRowSplitted[0] === TOTAL_SIGN) {
+        headerWithTotalProp.push(singleRow);
+      } else if (singleRowSplitted[1] === TOTAL_SIGN) {
+        mainCategoriesWithTotal.push(singleRow);
+      } else {
+        rowsContainsNestedTotalProp.push(singleRow);
+      }
+    } else {
+      // create array of nested properties / categories -> womens footwear|shoes
+      // extract rows have nested properties but not total property
+      const itemName = singleRowSplitted
+        .slice(0, propertiesCount - 1)
+        .join(COLUMN_SEPERATOR);
+      if (rowsWithoutTotalProp.has(itemName)) {
+        rowsWithoutTotalProp.set(itemName, [
+          ...rowsWithoutTotalProp.get(itemName),
+          singleRow,
+        ]);
+      } else {
+        rowsWithoutTotalProp.set(itemName, [singleRow]);
+      }
+    }
+  }
+  return {
+    rowsWithoutTotalProp,
+    rowsContainsNestedTotalProp,
+    mainCategoriesWithTotal,
+    headerWithTotalProp,
+    propertiesCount,
+  };
 }
 
 function getChildrensByMainCategory(hashTable, category, sortColumnIndex) {
@@ -143,32 +161,31 @@ function getMainCategories(mainCategoriesWithTotal) {
   return categories;
 }
 
+function writeToFile(fileName, content) {
+  fs.writeFileSync(fileName, content);
+}
+
 // calling it
 function main() {
   const inputFilename = argv[2];
   const sortColumn = argv[3];
+  const outputFilename = argv[4];
 
-  const fileContent = fs.readFileSync(inputFilename, "utf8");
+  const fileContent = fs.readFileSync(path.join(__dirname, inputFilename), "utf8");
   const rows = fileContent.split(LINE_SEPERATOR);
-  // console.log({
-  //   inputFilename,
-  //   sortColumn,
-  //   rows,
-  // });
+
   if (!rows[0].includes(sortColumn)) {
     console.log(
       "sort column not found in file header, please choose another column!"
     );
     return;
   }
-  hierarchicalSort(rows, sortColumn);
+  const sortedData = hierarchicalSort(rows, sortColumn);
+  console.log({ sortedData });
+
+  // const outputFilename = `data_sorted.csv`;
+  writeToFile(outputFilename, sortedData.join(LINE_SEPERATOR));
+  console.log(`${outputFilename} created!`);
 }
 
 main();
-
-// run ts file arg[0] input file name, arg[1] sort column name
-// parse text or csv file
-// pass rows to hierarchicalSort
-// sort rows by sort column arg[1]
-// write rowsContainsNestedTotalProp to output file.csv & .txt
-// TODO: try to sort in place
